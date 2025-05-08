79221d3998e3055afd451628825f2d674de1a917
orel-david <42713127+orel-david@users.noreply.github.com>
2025-05-09 00:05:13 +0300

    modify return format

--- verifier/LLM_gateway.py
+++ verifier/LLM_gateway.py
@@ -14,34 +14,42 @@ headers = {
     "Content-Type": "application/json"
 }
 
+def text_to_score(t):
+    if t == "yes":
+        return 4
+    elif t == "py":
+        return 3
+    elif t == "pn":
+        return 2
+    return 1
 
-def verify(claim, query, is_context=False):
-    context_dict = get_top_abstracts(query)
+def verify(claim, query, current_url, is_context=False):
+    source, context_dict = get_top_abstracts(query)
     payload = f"""
     Analyze the following context and claim. 
     context: {query if is_context else context_dict.values()}
     
     claim: {claim}
      answer in following format: only write: yes if the context implies the claim,
-     partially-yes if the context support the claim to a certain extent or if it gives reasonable doubt to support the claim,
-     partially-no if the context barely support claim,but there slight correlation and no direct contradiction,
+     py if the context support the claim to a certain extent or if it gives reasonable doubt to support the claim,
+     pn if the context barely support claim,but there slight correlation and no direct contradiction,
      no if the context and the claim contradict each-other or are not correlated.
     """
     payload = {
         "model": "meta-llama/llama-4-maverick:free",
         "messages": [
             {"role": "user", "content": payload}
+
         ],
         "max_tokens": 100
     }
-
     response = requests.post(API_URL, headers=headers, data=json.dumps(payload))
 
     if response.status_code == 200:
         response_content = response.json()['choices'][0]['message']['content']
-        print(response_content)
+        return {"title": current_url, "url": {source: {"connected_urls_list": [current_url], "score": text_to_score(response_content)}}}
     else:
         print("Error:", response.status_code, response.text)
 
 
-verify("vaccines are good", "vaccines are good")
+verify("vaccines are good", "vaccines are good", "example.com")
